4add836921e932798e2bbd3f48140b65a87e40bc
Fatbardh <fatbardh.kadriu@gmail.com>
2020-11-25 00:25:53 +0100

    Added default key and iv on ASP.NET

--- ASP.NET Application/AES_Encryption/Controllers/HomeController.cs
+++ ASP.NET Application/AES_Encryption/Controllers/HomeController.cs
@@ -26,8 +26,19 @@ namespace AES_Encryption.Controllers
         [HttpPost]
         public ActionResult Encryption(String plaintext, String key, String iv)
         {
-            Dictionary<String, String> parameters = new Dictionary<String, String> { { "Key", key }, { "IV", iv } };
-            List<String> results = EncryptAesManaged(plaintext, "e", parameters);
+            List<String> results = new List<string>();
+            Dictionary<String, String> parameters = null;
+            if (key != "" && iv != "")
+            {
+                parameters = new Dictionary<string, string>();
+                parameters.Add( "Key", key);
+                parameters.Add("IV", iv);
+                results = EncryptAesManaged(plaintext, "e", parameters);
+            }
+            else
+            {
+                results = EncryptAesManaged(plaintext, "e");
+            }
             // ViewBag.Message = "Your application encrypt page.";
             AESModel encryptModel = new AESModel();
             encryptModel.plaintext = plaintext;
@@ -59,7 +70,6 @@ namespace AES_Encryption.Controllers
         List<String> EncryptAesManaged(String data, String action, Dictionary<String, String> kwargs = null)
         {
             List<String> key_iv = new List<String>();
-
             try
             {
                 using (Aes myAes = Aes.Create())
@@ -69,40 +79,42 @@ namespace AES_Encryption.Controllers
                         string key = kwargs["Key"];
                         string iv = kwargs["IV"];
 
-                        HashAlgorithm hash = MD5.Create();
-
                         key_iv.Add(key);
                         key_iv.Add(iv);
 
-                        myAes.Key = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(key));
-                        myAes.IV = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(iv));
+                        if (key.Length != 44 && iv.Length != 24)
+                        {
+                            HashAlgorithm hash = MD5.Create();
 
+                            myAes.Key = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(key));
+                            myAes.IV = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(iv));
+
+                        }
+                        else
+                        {
+                            myAes.Key = Convert.FromBase64String(key);
+                            myAes.IV = Convert.FromBase64String(iv);
+
+                        }
                     }
-
-                    //Console.WriteLine($"Encryption key: {Convert.ToBase64String(myAes.Key)}");
-                    //Console.WriteLine($"Encryption IV: {Convert.ToBase64String(myAes.IV)}");
-
+                    else
+                    {
+                        key_iv.Add(Convert.ToBase64String(myAes.Key));
+                        key_iv.Add(Convert.ToBase64String(myAes.IV));
+                    }                 
 
                     if (action == "e")
                     {
-                        // Encrypt string    
                         byte[] encrypted = Encrypt(data, myAes.Key, myAes.IV);
-                        // Print encrypted string    
-                        //Console.WriteLine($"Encrypted data:{Convert.ToBase64String(encrypted)}");
+
                         key_iv.Add(Convert.ToBase64String(encrypted));
                     }
                     else
                     {
-                        // Decrypt string
                         string decrypted = Decrypt(Convert.FromBase64String(data), myAes.Key, myAes.IV);
-                        // Print decrypted string  
-                        // Console.WriteLine($"Decrypted data: {decrypted}");
                         key_iv.Add(decrypted);
                     }
-
-
                 }
-
             }
             catch (Exception exp)
             {
@@ -113,13 +125,6 @@ namespace AES_Encryption.Controllers
 
         byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
         {
-            // Check arguments.
-            if (plainText == null || plainText.Length <= 0)
-                throw new ArgumentNullException("plainText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
 
             byte[] encrypted;
 
@@ -128,73 +133,45 @@ namespace AES_Encryption.Controllers
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
 
-                // Create an encryptor to perform the stream transform.
                 ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
-                // Create the streams used for encryption.
                 using (MemoryStream msEncrypt = new MemoryStream())
                 {
-                    // Use a stream that links data streams to cryptographic transformations.
-                    // Create a CryptoStream, pass it the msEncrypt, and encrypt it with the Aes class encryptor. 
                     using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        // Create a StreamWriter for easy writing to the stream.
                         using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
                         {
-                            //Write all data to the stream.
                             swEncrypt.Write(plainText);
                         }
                         encrypted = msEncrypt.ToArray();
                     }
                 }
             }
-
-            // Return encrypted data    
             return encrypted;
         }
 
         string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
         {
-            // Check arguments.
-            if (cipherText == null || cipherText.Length <= 0)
-                throw new ArgumentNullException("cipherText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-
-            // Declare the string used to hold
-            // the decrypted text.
             string plaintext = null;
 
-            // Create an Aes object
-            // with the specified key and IV.
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
 
-                // Create a decryptor to perform the stream transform.
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                // Create the streams used for decryption.
                 using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
-                    // Use a stream that links data streams to cryptographic transformations.
-                    // Create a CryptoStream, pass it the msDecrypt, and dencrypt it with the Aes class decryptor. 
                     using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
-                        // Create a StreamRader for easy writing to the stream.
                         using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                         {
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
                             plaintext = srDecrypt.ReadToEnd();
                         }
                     }
                 }
             }
-            // Return decrypted data  
             return plaintext;
         }
     }
