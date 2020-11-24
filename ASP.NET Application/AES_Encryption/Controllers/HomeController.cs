0fd918b48480f3adb87f0b0720d5890f78d74362
Fatbardh <fatbardh.kadriu@gmail.com>
2020-11-24 01:17:50 +0100

    Established project structure

---
+++ ASP.NET Application/AES_Encryption/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using AES_Encryption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AES_Encryption.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+ 
+        public ActionResult Encryption()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Encryption(String plaintext, String key, String iv)
+        {
+            ViewBag.Message = "Your application encrypt page.";
+            AESModel encryptModel = new AESModel();
+            encryptModel.plaintext = plaintext;
+            encryptModel.ciphertext = "cipheri";
+            encryptModel.key = key;
+            encryptModel.iv = iv;
+            return View(encryptModel);
+        }
+
+        public ActionResult Decryption()
+        {
+            ViewBag.Message = "Your decrypt page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Decryption(String ciphertext, String key, String iv)
+        {
+            ViewBag.Message = "Your application encrypt page.";
+            AESModel encryptModel = new AESModel();
+            encryptModel.plaintext = "plaini";
+            encryptModel.ciphertext = ciphertext;
+            encryptModel.key = key;
+            encryptModel.iv = iv;
+            return View(encryptModel);
+        }
+
+    }
+}
---
+++ ASP.NET Application/AES_Encryption/Models/AESModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AES_Encryption.Models
+{
+    public class AESModel
+    {
+        public string plaintext { get; set; }
+
+        public string ciphertext { get; set; }
+
+        public string key { get; set; }
+
+        public string iv { get; set; }
+
+    }
+}
